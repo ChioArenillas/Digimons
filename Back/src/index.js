ce8a49448001b09f29d17b335f39ed43899ad21c
Rocio Arenillas (Chio) <arenillasr@gmail.com>
2025-12-12 11:36:44 +0100

    first commit

---
+++ Back/src/index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const cors = require('cors');
+const mongoose = require("mongoose");
+const digimonsRouter = require('./routes/digimonsRoutes');
+const app = express()
+const port = 9000
+
+const url_mongo = 'mongodb+srv://timiotull_db_user:I05XLEIv2PetwjSn@cluster0.uh5ltif.mongodb.net/?appName=Cluster0'
+
+mongoose.connect(url_mongo);
+
+// aquí hacemos la conexión con mongoose
+const db = mongoose.connection;
+
+// esto siguiente es para saber si ha habido algún error al conectarse a mongo
+db.on("error", (error) => {
+  console.log(`Error al conectar con mongo ${error}`);
+});
+// y esto es para saber si se ha conectado correctamente
+db.on("connected", () => {
+  console.log(`Succecss connect`);
+});
+ 
+db.on("disconected", () => {
+  console.log(`Mongo is disconected`);
+});
+
+app.use(express.json())
+app.use(cors());
+
+app.use('/digimons',digimonsRouter)
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
---
+++ Back/src/initialData/digimonsDB.js
@@ -0,0 +1,95 @@
+let digimonsDB = [
+  {
+    id: 1,
+    nombre: "Agumon",
+    tipo: "Reptil",
+    evolucion: "Greymon",
+  },
+  {
+    id: 2,
+    nombre: "Gabumon",
+    tipo: "Bestia",
+    evolucion: "Garurumon",
+  },
+  {
+    id: 3,
+    nombre: "Patamon",
+    tipo: "Mamífero",
+    evolucion: "Angemon",
+  },
+  {
+    id: 4,
+    nombre: "Gomamon",
+    tipo: "Anfibio",
+    evolucion: "Ikkakumon",
+  },
+  {
+    id: 5,
+    nombre: "Biyomon",
+    tipo: "Ave",
+    evolucion: "Birdramon",
+  },
+  {
+    id: 6,
+    nombre: "Tentomon",
+    tipo: "Insecto",
+    evolucion: "Kabuterimon",
+  },
+  {
+    id: 7,
+    nombre: "Palmon",
+    tipo: "Planta",
+    evolucion: "Togemon",
+  },
+  {
+    id: 8,
+    nombre: "Veemon",
+    tipo: "Dragon",
+    evolucion: "ExVeemon",
+  },
+  {
+    id: 9,
+    nombre: "Wormmon",
+    tipo: "Oruga",
+    evolucion: "Stingmon",
+  },
+  {
+    id: 10,
+    nombre: "Armadillomon",
+    tipo: "Mamífero",
+    evolucion: "Ankylomon",
+  },
+  {
+    id: 11,
+    nombre: "Hawkmon",
+    tipo: "Ave",
+    evolucion: "Aquilamon",
+  },
+  {
+    id: 12,
+    nombre: "Gatomon",
+    tipo: "Bestia",
+    evolucion: "Angewomon",
+  },
+  {
+    id: 13,
+    nombre: "Impmon",
+    tipo: "Demonio",
+    evolucion: "Beelzemon",
+  },
+  {
+    id: 14,
+    nombre: "Renamon",
+    tipo: "Zorro",
+    evolucion: "Kyubimon",
+  },
+  {
+    id: 15,
+    nombre: "Terriermon",
+    tipo: "Bestia",
+    evolucion: "Gargomon",
+  },
+];
+
+
+module.exports = digimonsDB
